17cd2a5fed02c95e39ad4930c3eeade2818c644c
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2023-03-21 17:51:05 +0100

    feat: day 2

---
+++ day2/day2.go
@@ -0,0 +1,64 @@
+package day2
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/jonasah/advent-of-code-2021/common"
+)
+
+type command struct {
+	direction string
+	steps     int
+}
+
+func Part1(input string) int {
+	commands := parseCommands(common.GetLines(input))
+
+	horizontal := 0
+	depth := 0
+	for _, command := range commands {
+		switch command.direction {
+		case "forward":
+			horizontal += command.steps
+		case "up":
+			depth -= command.steps
+		case "down":
+			depth += command.steps
+		}
+	}
+
+	return horizontal * depth
+}
+
+func Part2(input string) int {
+	commands := parseCommands(common.GetLines(input))
+
+	horizontal := 0
+	depth := 0
+	aim := 0
+	for _, command := range commands {
+		switch command.direction {
+		case "forward":
+			horizontal += command.steps
+			depth += aim * command.steps
+		case "up":
+			aim -= command.steps
+		case "down":
+			aim += command.steps
+		}
+	}
+
+	return horizontal * depth
+}
+
+func parseCommands(lines []string) []command {
+	commands := make([]command, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, " ")
+		direction := parts[0]
+		steps, _ := strconv.Atoi(parts[1])
+		commands[i] = command{direction, steps}
+	}
+	return commands
+}
---
+++ day2/day2_test.go
@@ -0,0 +1,28 @@
+package day2
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jonasah/advent-of-code-2021/common"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 150, Part1(testInput))
+	assert.Equal(t, 1893605, Part1(realInput))
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 900, Part2(testInput))
+	assert.Equal(t, 2120734350, Part2(realInput))
+}
+
+var realInput = common.GetInput(2)
+
+const testInput = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`
