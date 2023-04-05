2aca7e267abaf6bcedbe5d40bea472c4a275e33b
Jonas Andersson Hultgren <jonas.andersson00@gmail.com>
2023-04-05 19:40:38 +0200

    feat: day 6

---
+++ day6/day6.go
@@ -0,0 +1,51 @@
+package day6
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Part1(input string) int {
+	return doIt(input, 80)
+}
+
+func Part2(input string) int {
+	return doIt(input, 256)
+}
+
+func doIt(input string, numDays int) int {
+	buckets := parseInput(input)
+
+	for day := 0; day < numDays; day++ {
+		numNew := buckets[0]
+
+		for i := 1; i < 9; i++ {
+			buckets[i-1] = buckets[i]
+		}
+
+		buckets[6] += numNew
+		buckets[8] = numNew
+	}
+
+	return sum(buckets)
+}
+
+func parseInput(input string) [9]int {
+	timerStrs := strings.Split(input, ",")
+
+	buckets := [9]int{}
+	for _, timerStr := range timerStrs {
+		t, _ := strconv.Atoi(timerStr)
+		buckets[t]++
+	}
+
+	return buckets
+}
+
+func sum(buckets [9]int) int {
+	sum := 0
+	for _, v := range buckets {
+		sum += v
+	}
+	return sum
+}
---
+++ day6/day6_test.go
@@ -0,0 +1,23 @@
+package day6
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/jonasah/advent-of-code-2021/common"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 5934, Part1(testInput))
+	assert.Equal(t, 346063, Part1(realInput))
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 26984457539, Part2(testInput))
+	assert.Equal(t, 1572358335990, Part2(realInput))
+}
+
+var realInput = common.GetInput(6)
+
+const testInput = `3,4,3,1,2`
